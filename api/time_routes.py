546df645d240dccdb9edcb9199088adca1ab13f7
RSTEIN2525 <ryan.andrew.stein@gmail.com>
2025-04-23 08:35:39 -0400

    v.1.0.7

--- api/time_routes.py
+++ api/time_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
-from sqlmodel import Session
-from datetime import datetime
+from sqlmodel import Session, select
+from datetime import datetime, timezone
 from models.time_log import PunchType, TimeLog, PunchRequest
 from db.session import get_session
 from services.punch_service import PunchService
@@ -33,3 +33,44 @@ def clock_out(data: PunchRequest, session: Session = Depends(get_session)):
         longitude=data.longitude,
         session=session,
     )
+
+
+# Get Todays Logs
+@router.get("/today")
+def get_todays_logs(employee_id: str, session=Depends(get_session)):
+    now = datetime.now(timezone.utc)
+    start_of_day = now.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    punches = session.exec(
+        select(TimeLog)
+        .where(TimeLog.employee_id == employee_id)
+        .where(TimeLog.timestamp >= start_of_day)
+        .order_by(TimeLog.timestamp.asc())
+    ).all()
+    return {"status": "success", "data": punches}
+
+
+# Get All Logs
+@router.get("/logs")
+def get_all_logs(employee_id: str, session: Session = Depends(get_session)):
+    punches = session.exec(
+        select(TimeLog)
+        .where(TimeLog.employee_id == employee_id)
+        .order_by(TimeLog.timestamp.desc())
+    ).all()
+    return {"status": "success", "data": punches}
+
+
+# Get Most Recent Punch
+@router.get("/last-punch")
+def get_last_punch(employee_id: str, session: Session = Depends(get_session)):
+    last_punch = session.exec(
+        select(TimeLog)
+        .where(TimeLog.employee_id == employee_id)
+        .order_by(TimeLog.timestamp.desc())
+    ).first()
+
+    if not last_punch:
+        return {"status": "success", "data": None, "message": "No punches found."}
+
+    return {"status": "success", "data": last_punch}
